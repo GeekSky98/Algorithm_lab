6f57193fcb244a08eb6c31eb2603dd9cdcdcd19d
Deepsky <deepskyai1215@gmail.com>
2023-03-21 17:21:14 +0900

    baekjoon 010

---
+++ Baekjoon/010.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/1946
+
+import sys
+
+input = sys.stdin.readline
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    rank = [list(map(int, input().split())) for _ in range(N)]
+    rank_asc = sorted(rank)
+    top = 0
+    result = 1
+
+    for i in range(1, len(rank_asc)):
+        if rank_asc[i][1] < rank_asc[top][1]:
+            top = i
+            result += 1
+
+    print(result)
